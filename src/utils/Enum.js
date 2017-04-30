fc461933947302c31acfc265236d6a2ab0e016d1
Ashton Six <me@ashtonsix.com>
2017-04-30 05:36:25 +0100

    Rewrite Enum as class

--- src/utils/Enum.js
+++ src/utils/Enum.js
@@ -1,61 +1,52 @@
-export const Enum = (arr, mapper) => {
-  let keys;
-
-  if (arr instanceof Array) {
-    if (typeof mapper === 'string') {
-      const _mapper = mapper;
-      mapper = value => value[_mapper];
-    }
-
-    keys = [];
-    arr = arr.slice();
-
-    arr.forEach((value, i) => {
-      const key = mapper(value, i);
-      keys.push(key);
-      arr[key] = arr[i];
-    });
-  } else {
-    keys = Object.keys(arr);
-    arr = Object.assign([], arr);
-    keys.forEach((key, i) => {
-      arr[i] = arr[key];
-    });
+export class Enum extends Array {
+  constructor(obj) {
+    const keys = Object.keys(obj);
+    const values = keys.map(key => obj[key]);
+    super(...values);
+    this.keys = keys;
+    keys.forEach(key => (this[key] = obj[key]));
   }
-
-  const methods = {};
-
-  const methodNames = ['every', 'find', 'findIndex', 'forEach', 'some'];
-  methodNames.forEach(name => {
-    methods[name] = arr[name].bind(arr);
-    arr[name] = f =>
-      methods[name]((value, index, arr) => f(value, index, keys[index], arr));
-  });
-
-  const reduceMethodNames = ['reduce', 'reduceRight'];
-  reduceMethodNames.forEach(name => {
-    methods[name] = arr[name].bind(arr);
-    arr[name] = f =>
-      methods[name]((v, pv, index, arr) => f(v, pv, index, keys[index], arr));
-  });
-
-  const map = arr.map.bind(arr);
-  arr.map = f =>
-    new Enum(map((value, index, arr) => f(value, index, arr, keys[index])));
-
-  arr.filter = f => {
-    const values = [];
-    const keys = [];
-    arr.forEach((value, index, key) => {
-      if (f(value, index, key)) {
-        values.push(value);
-        keys.push(key);
+  map(f) {
+    const obj = {};
+    this.forEach((value, index, key, arr) => {
+      obj[key] = f(value, index, key, arr);
+    });
+    return new Enum(obj);
+  }
+  filter(f) {
+    const obj = {};
+    this.forEach((value, index, key, arr) => {
+      if (f(value, index, key, arr)) {
+        obj[key] = value;
       }
     });
-    return new Enum(values, (v, i) => keys[i]);
-  };
+    return new Enum(obj);
+  }
+  forEach(f) {
+    return super.forEach((value, index, arr) => f(value, index, this.keys[index], arr));
+  }
+  every(f) {
+    return super.every((value, index, arr) => f(value, index, this.keys[index], arr));
+  }
+  find(f) {
+    return super.find((value, index, arr) => f(value, index, this.keys[index], arr));
+  }
+  findIndex(f) {
+    return super.findIndex((value, index, arr) => f(value, index, this.keys[index], arr));
+  }
+  some(f) {
+    return super.some((value, index, arr) => f(value, index, this.keys[index], arr));
+  }
+  reduce(f) {
+    return super.reduce((prev, value, index, arr) =>
+      f(prev, value, index, this.keys[index], arr)
+    );
+  }
+  reduceRight(f) {
+    return super.reduceRight((prev, value, index, arr) =>
+      f(prev, value, index, this.keys[index], arr)
+    );
+  }
+}
 
-  Object.freeze(arr);
-
-  return arr;
-};
+export default Enum;
